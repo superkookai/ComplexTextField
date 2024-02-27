eadd12c663095e27c4ea8cb614d2ef42e01f91ac
Weerawut Chaiyasomboon <weerawut@live.com>
2024-02-27 14:24:00 +0700

    Initial Commit

---
+++ ComplexTextField/ComplexTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexTextFieldApp.swift
+//  ComplexTextField
+//
+//  Created by Weerawut Chaiyasomboon on 27/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ComplexTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
